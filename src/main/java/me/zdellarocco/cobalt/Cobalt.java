f09482f4415eaf947aaba880f0ab9a69a7f0f46e
Zack <dellaroccozack@gmail.com>
2021-08-16 22:15:44 -0400

    Added /modeAll command.

--- src/main/java/me/zdellarocco/cobalt/Cobalt.java
+++ src/main/java/me/zdellarocco/cobalt/Cobalt.java
@@ -1,9 +1,6 @@
 package me.zdellarocco.cobalt;
 
-import me.zdellarocco.cobalt.commands.CreditCMD;
-import me.zdellarocco.cobalt.commands.GamemodeCMD;
-import me.zdellarocco.cobalt.commands.delTNTCMD;
-import me.zdellarocco.cobalt.commands.helpCMD;
+import me.zdellarocco.cobalt.commands.*;
 import me.zdellarocco.cobalt.listeners.blockPlaceEvent;
 import me.zdellarocco.cobalt.listeners.playerJoinMsg;
 import me.zdellarocco.cobalt.listeners.playerLeaveMSG;
@@ -22,11 +19,12 @@ public final class Cobalt extends JavaPlugin {
         // Listeners //
         this.getServer().getPluginManager().registerEvents(new playerJoinMsg(), this); // Player Join Event
         this.getServer().getPluginManager().registerEvents(new playerLeaveMSG(), this); // Player Quit Event
-        this.getServer().getPluginManager().registerEvents(new blockPlaceEvent(), this);
+        this.getServer().getPluginManager().registerEvents(new blockPlaceEvent(), this); // Block Place Event
 
         // Commands //
         Objects.requireNonNull(this.getCommand("credits")).setExecutor(new CreditCMD());
         Objects.requireNonNull(this.getCommand("mode")).setExecutor(new GamemodeCMD());
+        Objects.requireNonNull(this.getCommand("modeall")).setExecutor(new modeAll());
         Objects.requireNonNull(this.getCommand("deltnt")).setExecutor(new delTNTCMD());
         Objects.requireNonNull(this.getCommand("chelp")).setExecutor(new helpCMD());
 
